b59dc908eb79bec830a8a8039d7c3829e0b585fe
RedNeath <theandre.bourry@ik.me>
2025-10-08 23:58:26 +0200

    [FEAT] API:Plans:addRecord

--- Controllers/API/ReferenceController.cs
+++ Controllers/API/ReferenceController.cs
@@ -1220,5 +1220,124 @@ namespace CarCareTracker.Controllers.API
                 PartialViewName = "/Views/API/Reference/Vehicles/Plans/_Index.cshtml"
             });
         }
+        
+        [Route("/API/Reference/Vehicles/Plans/AddRecord")]
+        public IActionResult VehiclesPlansAddRecord()
+        {
+            return View("/Views/API/Reference/Base.cshtml", new BaseReferenceViewModel()
+            {
+                Name = "Add plan record",
+                PartialViewName = "/Views/API/Reference/Vehicles/Plans/_AddRecord.cshtml",
+                Endpoint = new ReferenceEndpointViewModel()
+                {
+                    Method = HttpMethod.Post,
+                    Route = "/api/vehicle/planrecords/add",
+                    RouteParameters = [
+                        new ReferenceEndpointRouteParameterViewModel()
+                        {
+                            Name = "vehicleId",
+                            Type = "int",
+                            Description = "Id of vehicle",
+                            IsRequired = true,
+                            Example = "1"
+                        }
+                    ],
+                    Body = new ReferenceEndpointFormDataBodyViewModel()
+                    {
+                        Content = [
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "description",
+                                Description = "Description",
+                                Example = "\"Install the new exhaust line\"",
+                                IsRequired = true,
+                                Type = "string",
+                                DisplayExample = true
+                            },
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "cost",
+                                Description = "Cost ⚠️: Locale sensitive",
+                                Example = "822.99",
+                                IsRequired = true,
+                                Type = "float",
+                                DisplayExample = true
+                            },
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "type",
+                                Description = "One of: `ServiceRecord`, `RepairRecord` or `UpgradeRecord`.",
+                                Example = "\"UpgradeRecord\"",
+                                IsRequired = true,
+                                Type = "string",
+                                DisplayExample = true
+                            },
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "priority",
+                                Description = "One of: `Low`, `Normal` or `Critical`.",
+                                Example = "\"Normal\"",
+                                IsRequired = true,
+                                Type = "string",
+                                DisplayExample = true
+                            },
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "progress",
+                                Description = "One of: `Backlog`, `InProgress` or `Testing`",
+                                Example = "\"Backlog\"",
+                                IsRequired = true,
+                                Type = "string",
+                                DisplayExample = true
+                            },
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "notes",
+                                Description = "Notes",
+                                Example = "\"Remember checking that sound doesn't level exceed the 84 db legal limit.\"",
+                                IsRequired = false,
+                                Type = "string",
+                                DisplayExample = true
+                            },
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "extrafields",
+                                Description = "See format for extra fields",
+                                Example = "",
+                                IsRequired = false,
+                                Type = "string",
+                                DisplayExample = false
+                            },
+                            new ReferenceEndpointFormDataPropertyViewModel()
+                            {
+                                Name = "files",
+                                Description = "Json response from the Upload document route",
+                                Example = """
+                                "[
+                                    {
+                                        name: \"plan.jpg\",
+                                        location: \"/documents/bc28e7d5-a533-4108-b184-3cdc77f23c12.jpg\",
+                                        isPending: false
+                                    }
+                                ]"
+                                """,
+                                IsRequired = false,
+                                Type = "string",
+                                DisplayExample = true
+                            },
+                        ]
+                    },
+                    Response = new ReferenceEndpointJsonResponseViewModel()
+                    {
+                        Content = """
+                        {
+                            "success": true,
+                            "message": "Plan Record Added"
+                        }
+                        """
+                    }
+                }
+            });
+        }
     }
 }
